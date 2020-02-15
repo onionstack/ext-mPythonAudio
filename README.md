5426571fe928fde0a47896d7cf8ddbb5c2b6b6c3
L-eeX <1035868977@qq.com>
2020-02-15 16:35:28 +0800

    lx

---
+++ README.md
@@ -0,0 +1,32 @@
+# Audio
+
+掌控音频模块
+
+# 用户库教程
+
+教程链接：[Mind+官方文档](https://mindplus.dfrobot.com.cn/extensions-user).
+
+---------------------------------------------------------
+
+## Table of Contents
+
+* [URL](#url)
+* [Summary](#summary)
+* [License](#license)
+* [Supported targets](#Supportedtargets)
+
+## URL
+project URL: ```https://github.com/onionstack/ext-mPythonAudio```
+
+## Summary
+掌控音频用户扩展库，包含音频播放、录制以及语音指令识别功能。
+
+## License
+
+MIT
+
+## Supported targets
+
+MCU                | JavaScript    | Arduino   | MicroPython    | Remarks
+------------------ | :----------: | :----------: | :---------: | -----
+mpython        |             |        √      |             | 
---
+++ arduinoC/main.ts
@@ -0,0 +1,86 @@
+
+enum CONTROL {
+    //% block="PAUSE"
+    PAUSE,
+    //% block="GO ON"
+    RESUME,
+    //% block="STOP"
+    STOP
+}
+
+
+//% color="#AA278D" iconWidth=50 iconHeight=40
+namespace mPythonAudio {
+    //% block="initialize Audio" blockType="command"
+    export function AudioPlayerInit(parameter: any, block: any) {
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode('audio.playerInit();');
+    }
+
+    //% block="deinit Audio" blockType="command"
+    export function AudioPlayerDeinit(parameter: any, block: any) {
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode('audio.playerDeinit();');
+    }
+
+    //% block="set Audio volume as [VOLUME]" blockType="command"
+    //% VOLUME.shadow="number" VOLUME.defl=100
+    export function AudioSetVolume(parameter: any, block: any) {
+        let volume = parameter.VOLUME.code;
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode(`audio.setVolume(${volume});`);
+    }
+
+    //% block="Audio [CONTROL]" blockType="command"
+    //% CONTROL.shadow="dropdown" CONTROL.options="CONTROL" CONTROL.defl="CONTROL.PAUSE"
+    export function AudioControl(parameter: any, block: any) {
+        let control = parameter.CONTROL.code;
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode(`audio.control(audio.${control});`);
+    }
+    
+    //% block="Audio play [PATH]" blockType="command"
+    //% PATH.shadow="string" PATH.defl=http://wiki.labplus.cn/images/4/4e/Music_test.mp3
+    export function AudioPlay(parameter: any, block: any) {
+        let path = parameter.PATH.code;
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode(`audio.play(${path});`);
+    }
+    
+    //% block="initialize Recording" blockType="command"
+    export function AudioRecorderInit(parameter: any, block: any) {
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode('audio.recorderInit();');
+    }
+
+    //% block="record audio with storage path [PATH] and duration [TIME] second(s)" blockType="command"
+    //% PATH.shadow="string" PATH.defl=1.wav
+    //% TIME.shadow="number" TIME.defl=2
+    export function AudioRecord(parameter: any, block: any) {
+        let path = parameter.PATH.code;
+        let time = parameter.TIME.code;
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode(`audio.record(${path}, ${time});`);
+    }
+
+    //% block="trelease cache of recording" blockType="command"
+    export function AudioRecorderDeinit(parameter: any, block: any) {
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode('audio.recorderDeinit();');
+    }
+
+    //% block="get voice command within [TIME] seconds" blockType="reporter"
+    export function AudioGetAsrResult(parameter: any, block: any) {
+        Generator.addInclude('MPython_Audio', '#include <MPython_Audio.h>');
+        Generator.addObject(`audio`, `MPython_Audio`, `audio;`);
+        Generator.addCode(['audio.getAsrResult()', Generator.ORDER_ATOMIC]);
+    }
+}
